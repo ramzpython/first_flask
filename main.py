92e4ff49175204077b705df827180a462676a13b
Ramunas Pl <destytojas@pythonkursas.smshostingas.lt>
2022-09-14 00:31:27 +0300

    su kintamuoju

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<h1>Čia mano naujas puslapis</h1>"
+@app.route("/<vardas>")
+def user(vardas):
+    return f"Labas, {vardas}"
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
